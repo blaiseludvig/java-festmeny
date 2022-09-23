fa91d1c3b3b9790ee2a77c3b9117a1b4896597bc
blaiseludvig <blaiseludvig@gmail.com>
2022-09-23 15:39:19 +0200

    create Festmeny class and declare fields

---
+++ src/hu/petrik/festmeny/Festmeny.java
@@ -0,0 +1,15 @@
+package hu.petrik.festmeny;
+
+import java.time.LocalDateTime;
+
+public class Festmeny {
+    private String cim, festo, stilus;
+
+    private int licitekSzama = 0;
+    private int legmagasabbLicit = 0;
+
+    private LocalDateTime legutolsoLicitIdeje = null;
+
+    private boolean elkelt = false;
+
+}
