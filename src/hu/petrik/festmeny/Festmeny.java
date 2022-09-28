904f8854b62e7c6d6f9696a74896184a5f4f881c
blaiseludvig <blaiseludvig@gmail.com>
2022-09-28 23:26:14 +0200

    declare Festmeny class methods

--- src/hu/petrik/festmeny/Festmeny.java
+++ src/hu/petrik/festmeny/Festmeny.java
@@ -12,4 +12,47 @@ public class Festmeny {
 
     private boolean elkelt = false;
 
+    Festmeny(String cim, String festo, String stilus) {
+        this.cim = cim;
+        this.festo = festo;
+        this.stilus = stilus;
+    }
+
+
+    public String getFesto() {
+        return festo;
+    }
+
+    public String getStilus() {
+        return stilus;
+    }
+
+    public int getLicitekSzama() {
+        return licitekSzama;
+    }
+
+    public int getLegmagasabbLicit() {
+        return legmagasabbLicit;
+    }
+
+    public LocalDateTime getLegutolsoLicitIdeje() {
+        return legutolsoLicitIdeje;
+    }
+
+    public boolean getElekelt() {
+        return elkelt;
+    }
+
+    public void setElkelt(boolean value) {
+        elkelt = value;
+    }
+
+    public void licit() {
+
+    }
+
+    public void licit(int licit) {
+
+    }
+
 }
